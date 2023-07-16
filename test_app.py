d2a19fd9c1565b2b336d417536cb932d3ece378a
Nazim22 <34912639+Nazim22@users.noreply.github.com>
2023-07-16 02:18:01 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,43 +1,34 @@
 # test_app.py
-import unittest
 import streamlit as st
-from StreamlitClient import StreamlitClient
+import app  # Assuming your Streamlit app script is named app.py
 
+# Import the pytest_streamlit fixture
+from pytest_streamlit import streamlit_tester
 
-class TestApp(unittest.TestCase):
-    def setUp(self):
-        # Create a Streamlit client instance
-        self.client = StreamlitClient('app.py')
+def test_prediction():
+    # Define test inputs
+    test_inputs = {
+        'What is the current Ex-Showroom price of the car': 10.0,
+        'What is the distance completed by the car in Miles?': 5000,
+        'What is the Fuel type of the car?': 'Petrol',
+        'Are you a Dealer or Individual?': 'Dealer',
+        'What is the Transmission Type?': 'Manual',
+        'Number of Owners the car previously had?': 1,
+        'In which year car was purchased?': 2018
+    }
 
-    def test_prediction(self):
-        # Define test inputs
-        test_inputs = {
-            'What is the current Ex-Showroom price of the car': 10.0,
-            'What is the distance completed by the car in Miles?': 5000,
-            'What is the Fuel type of the car?': 'Petrol',
-            'Are you a Dealer or Individual?': 'Dealer',
-            'What is the Transmission Type?': 'Manual',
-            'Number of Owners the car previously had?': 1,
-            'In which year car was purchased?': 2018
-        }
-
+    # Simulate the Streamlit app's behavior using Pytest-Streamlit
+    with streamlit_tester(runner=app):
         # Set the inputs for the test
-        self.client.set_input(test_inputs)
+        for key, value in test_inputs.items():
+            st.text_input(key, value)
 
         # Simulate clicking the 'Predict' button
-        self.client.submit_form('Predict')
+        st.button('Predict')
 
         # Get the result of the prediction
-        prediction_result = self.client.get_element_text('You can sell your car for')
+        prediction_result = st.text('You can sell your car for')
 
         # Ensure the result is as expected
         expected_result = 'You can sell your car for 5.00 lakhs'
-        self.assertEqual(prediction_result, expected_result)
-
-    def tearDown(self):
-        # Close the Streamlit app after each test
-        self.client.close()
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert prediction_result == expected_result
